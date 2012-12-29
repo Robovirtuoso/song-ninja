0073b0999eef5e1ec6a766b2e1c0b275e53cec58
Alex Williams <alexwilliams4@me.com>
2012-12-29 15:59:13 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SongNinja::Application.config.secret_token = '8ef5db1e8ebf8ebe3ad298edb2d9af480ce274393acd0d2bb3df011a668591fc4b243841a2edd47db9e4c79d9790fe1d233b7ed6caa712692702b5917175875b'
