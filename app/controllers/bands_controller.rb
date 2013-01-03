0f42677d7cfc38557aaf291636fff815ffcd6923
Alex Williams <alexwilliams4@me.com>
2013-01-02 19:00:25 -0700

    Added a remove song link and am sorting the bands by name

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -2,4 +2,10 @@ class BandsController < ApplicationController
   inherit_resources
 
   respond_to :html, :json, :js
+
+  def index
+    index! do
+      @bands.sort_by! { |band| band.name }
+    end
+  end
 end
