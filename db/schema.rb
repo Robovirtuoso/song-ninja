dcf1e9d75ab2d30491dad841c9c9aa643dd104c4
Alex Williams <alexwilliams4@me.com>
2013-01-05 13:56:01 -0700

    Added the relationship between song and album

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130105202705) do
+ActiveRecord::Schema.define(:version => 20130105205142) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130105202705) do
     t.integer  "band_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "album_id"
   end
 
   add_index "songs", ["band_id"], :name => "index_songs_on_band_id"
